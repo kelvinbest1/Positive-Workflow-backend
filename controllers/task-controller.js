98831bc6c96fea279f33445bcf35fc9918d8eb0e
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-10 12:44:57 -0400

    adding Task create and index function

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -1,16 +1,38 @@
 const { Task } = require('../models')
 
-
-async function create(req, res,next){
+async function index(req, res, next) {
     try {
-      const newTask = new Task(req.body);
-      await newTask.save();
-      res.status(200).json({
-          message: "Task created successfully",
-          data: newTask,
+        Object.keys(req.body).forEach((key) => {
+            if (req.body[key] === "all") {
+                delete req.body[key];
+            }
         });
-      } catch (err) {
+        delete req.body["userId"];
+        const tasks = await Task.find(req.body)
+            .populate("assignedTo")
+            .populate("assignedBy")
+            .populate("project")
+            .sort({ createdAt: -1 });
+        res.status(201).json({
+            message: "Tasks fetched successfully",
+            data: tasks,
+        });
+    } catch (err) {
         res.status(400).json({ error: err.message })
-      }
-    };
-       
+    }
+};
+
+
+async function create(req, res, next) {
+    try {
+        const newTask = new Task(req.body);
+        await newTask.save();
+        res.status(201).json({
+            message: "Task created successfully",
+            data: newTask,
+        });
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+};
+
