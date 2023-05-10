bb67e5515a716b560a7039baefcdfdcf3e1e0286
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-10 11:37:18 -0400

    complete User model

--- controllers/models/User.js
+++ controllers/models/User.js
@@ -10,3 +10,18 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    },
+    { timestamps: true }
+  );
+  
+  module.exports = mongoose.model("users", userSchema);
