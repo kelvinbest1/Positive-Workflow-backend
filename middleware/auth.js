73cc04bcea6a5e61056d182c06eb575af76b4111
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-11 13:12:33 -0400

    updated middleware auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,82 +1,15 @@
-///////////////////////////////
-// DEPENDENCIES
-////////////////////////////////
-
-// Require the needed npm packages
-const passport = require('passport')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {User} = require('../models')
-
-
-const { Strategy, ExtractJwt } = require('passport-jwt')
-
-
-///////////////////////////////
-// CONFIGURATION
-////////////////////////////////
-
-
-const secret = process.env.JWT_SECRET || 'kelvin'
-
-
-
-const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: secret
-}
-
-///////////////////////////////
-// AUTHENTICATION FUNCTIONALITY
-////////////////////////////////
-
-const verify = async (jwt_payload, done) => {
-
-    try {
-        const user = await User.findById(jwt_payload.id)
-        return done(null, user)
-    }catch(err){
-       return done(err)
-    }
-
-}
-
-const strategy = new Strategy(opts,verify)
-
-
-passport.use(strategy)
-
-passport.initialize()
-
-const requireToken = passport.authenticate('jwt', {session: false})
-
-const createUserToken = (req, user) => {
-
-		if(
-			!user ||
-			!req.body.password ||
-			!bcrypt.compareSync(req.body.password, user.password)
-			){
-	        const error = new Error("The provided username or password is incorrect")
-	        error.statusCode = 422
-	        throw error
-    }
-
-    return jwt.sign({id: user._id}, secret, {expiresIn: 36000 })
-}
-
-const handleValidateOwnership =(req,document) => {
-    const ownerId = document.owner._id || document.owner 
-
-    if(!req.user._id.equals(ownerId)){
-        throw Error('Unauthorized Access')
-    }else {
-        return document 
-    }
-}
-
-module.exports = {
-    requireToken,
-    createUserToken,
-    handleValidateOwnership
-}
+const jwt = require("jsonwebtoken");
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decryptedToken = jwt.verify(token, process.env.jwt_secret);
+    req.body.userId = decryptedToken.userId;
+    next();
+  } catch (error) {
+    
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
