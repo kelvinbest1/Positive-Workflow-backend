a3e022607f721d0fc134067765410fc100185c1d
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-11 14:40:37 -0400

    updating middleware

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const User = require("../models/userModel");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/auth");
 
 // register a new user
 router.post("/register", async (req, res) => {
